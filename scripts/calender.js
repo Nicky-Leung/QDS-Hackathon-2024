906ed6f8dfda01bd0cc1d4489e4fa62dea19fc16
Nicky Leung <nicky-leung@outlook.com>
2024-03-09 09:14:44 -0800

    add skeleton to calender page

--- scripts/calender.js
+++ scripts/calender.js
@@ -3,6 +3,7 @@ loadtemplate = function() {
 
     $('#navbar').load('./templates/nav.html');
     $('#calender').load('./templates/calendercard.html');
+    $('#tasks').load('./templates/taskcard.html');
 
     $('#footer').load('./templates/footer.html');
 
