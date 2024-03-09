1bc335f46ac4e98cbdbc068e9bc95c4f68736f4b
Nicky Leung <nicky-leung@outlook.com>
2024-03-09 09:54:56 -0800

    start sleep card

--- scripts/overview.js
+++ scripts/overview.js
@@ -2,7 +2,7 @@
 loadtemplate = function() {
 
     $('#navbar').load('./templates/nav.html');
-    $('#healthcard').load('./templates/healthcard.html');
+    $('#healthcard').load('./templates/sleepcard.html');
     $('#assignmentcard').load('./templates/assignmentcard.html');
     $('#footer').load('./templates/footer.html');
 
