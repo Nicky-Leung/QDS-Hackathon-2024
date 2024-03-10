ec98e83e8cf7f0d6e606863a28d717a8ed45a269
persephone-wong <persephone.w315@gmail.com>
2024-03-09 18:31:31 -0800

    Changed the calender to be a full page calender , added a new task card, and added a new footer.

--- scripts/calender.js
+++ scripts/calender.js
@@ -1,7 +1,6 @@
 
 loadtemplate = function() {
 
-    $('#navbar').load('./templates/nav.html');
     $('#calender').load('./templates/calendercard.html');
     $('#tasks').load('./templates/taskcard.html');
     $('#footer').load('./templates/footer.html');
